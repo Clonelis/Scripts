02966661000af425a870357224ee03aded65ca5c
Clonelis <daqa2083@gmail.com>
2025-05-10 00:36:36 +0300

    white list.lua

---
+++ white list.lua
@@ -0,0 +1,9 @@
+local supportedGameId = 1488
+if currentGameId ~= supportedGameId then
+    warn("ROBLOX CRASHES BY CLONELIS")
+    
+    while true do end
+    script:Destroy()
+    
+    return
+end
